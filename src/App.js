b3f6be2242a4276ae8a05632189b99e8f63f6d8b
christiannwamba <nwambachristian@gmail.com>
2022-10-04 08:17:14 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  CreateNote,
+  NavBar,
+  NoteUICollection,
+  UpdateNote,
+} from './ui-components';
 
-function App() {
+import React from 'react';
+
+import { Hub } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { DataStore } from "aws-amplify";
+
+function App({signOut}) {
+  const [showCreateModel, setShowCreateModel] = React.useState(false);
+  const [showUpdateModel, setShowUpdateModel] = React.useState(false);
+  const [noteToUpdate, setNoteToUpdate] = React.useState();
+
+  React.useEffect(() => {
+    Hub.listen('ui', (capsule) => {
+      if (capsule.payload.event === 'actions:datastore:create:finished') {
+        setShowCreateModel(false);
+      }
+      if (capsule.payload.event === 'actions:datastore:update:finished') {
+        setShowUpdateModel(false);
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar
+        width="100%"
+        marginBottom="20px"
+        overrides={{
+          Button31632483: { onClick: () => setShowCreateModel(true) },
+
+          Button31632487: {
+            onClick: async () => {
+              signOut();
+              await DataStore.clear();
+            },
+          },
+        }}
+      />
+      <div className="container">
+        <NoteUICollection
+          overrideItems={({ item, idx }) => {
+            return {
+              overrides: {
+                Vector31472745: {
+                  onClick: () => {
+                    console.log(item);
+                    setShowUpdateModel(true);
+                    setNoteToUpdate(item);
+                  },
+                },
+              },
+            };
+          }}
+        />
+      </div>
+
+      <div
+        className="modal"
+        style={{ display: showCreateModel === false && 'none' }}
+      >
+        <CreateNote
+          overrides={{
+            MyIcon: { as: 'button', onClick: () => setShowCreateModel(false) },
+          }}
+        />
+      </div>
+      <div
+        className="modal"
+        style={{ display: showUpdateModel === false && 'none' }}
+      >
+        <UpdateNote
+          note={noteToUpdate}
+          overrides={{
+            MyIcon: { as: 'button', onClick: () => setShowUpdateModel(false) },
+            // Button: { onClick: () => setShowUpdateModel(false)}
+          }}
+        />
+      </div>
+    </>
   );
 }
-
-export default App;
+export default withAuthenticator(App);
